69d7327a7421fc73c4d2b611adba566d12c5ba6b
Allaeddine Khalid <allaeddinekhalid@gmail.com>
2020-01-17 11:54:38 +0100

    ft_printf now works perfectly

--- unsignedint.c
+++ unsignedint.c
@@ -6,7 +6,7 @@
 /*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/04 04:14:47 by akhalid           #+#    #+#             */
-/*   Updated: 2020/01/08 20:19:33 by akhalid          ###   ########.fr       */
+/*   Updated: 2020/01/17 11:36:35 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	parse_unsigned_int(t_args *args)
 {
 	MINUS = PRECISION < 0 ? 1 : MINUS;
 	WIDTH = (PRECISION < 0) ? PRECISION * -1 : WIDTH;
+	PRECISION -= LEN;
 	WIDTH -= PRECISION > 0 ? PRECISION + LEN : LEN;
 	ZERO = IFPREC == 1 ? 0 : ZERO;
 }
@@ -27,18 +28,16 @@ void	print_unsigned_int(t_args *args, va_list pa)
 
 	nb = assign_value(args, pa);
 	print = convert(args, nb);
-	LEN = nb == 0 ? 0 : ft_strlen(print);
+	LEN = ft_strlen(print);
 	parse_unsigned_int(args);
-	if (MINUS <= 0 && WIDTH <= 0 && PRECISION <= 0 && IFPREC <= 0 && ZERO <= 0)
-		ft_putstrpr(print, args);
 	if (MINUS == 0)
 		while (WIDTH-- > 0)
 			ZERO == 1 ? ft_putcharpr('0', args) : ft_putcharpr(' ', args);
 	while (PRECISION-- > 0)
 		ft_putcharpr('0', args);
+	ft_putstrpr(print, args);
+	WIDTH = WIDTH < 0 ? WIDTH * -1 - (2 * LEN + 1) : WIDTH * 1;
 	while (WIDTH-- > 0)
 		ft_putcharpr(' ', args);
-	if ((MINUS > 0 || WIDTH > 0 || PRECISION > 0 || IFPREC > 0 || ZERO > 0) && LEN > 0)
-		ft_putstrpr(print, args);
 	free(print);
 }
