fa0ab04b97f2447bf32204fde8f1b6f29977f7b7
eniddealla <allaeddinekhalid@gmail.com>
2020-01-20 08:57:22 +0100

    adding tester

---
+++ tester/run.sh
@@ -0,0 +1,6 @@
+rm -rf EXPECTED OUT TESTS deepthought
+gcc -Wall -Wextra -Werror printf_main.c ../libftprintf.a
+./a.out > OUT
+gcc -Wall -Wextra -Werror main.c gnl.a
+./a.out
+rm -rf EXPECTED OUT TESTS ./a.out
